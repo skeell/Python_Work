3f386204ddc5cbf2bd2da10534efc4668bb26a7a
skeell <musocchi.pietro@gmail.com>
2022-06-09 14:30:00 +0200

    9 giugno

---
+++ numpy/index.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+arr = np.array([1,2,3,4,5])
+print(arr * 5)
+print(arr + 5)
+
+arrArange = np.arange(0,91)
+print(arrArange)
+
+arrZeros = np.zeros(5)
+print(arrZeros)
+
+arrZeros1 = np.zeros((1,3,5))
+print(arrZeros1)
