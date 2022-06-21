c8d09016731c38369699341bea6ad643cb5e1529
skeell <musocchi.pietro@gmail.com>
2022-06-21 11:21:07 +0200

    21

---
+++ Tkinter/label.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+my_Window = Tk()
+label_1 = Label(my_Window,
+                relief ="solid",
+                font="Times 22 bold")
+
+label_1.pack()
+my_Window.mainloop()
+                
